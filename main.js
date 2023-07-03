9da91a040b3a106ba84efe9c4209d54b404d0628
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-07-02 23:20:19 -0400

    progress on action sections

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 
 
+let actionSectionBarriers = []
+
+let actionSections = document.querySelectorAll(".actionSection")
+
+function calculateBarriers() {
+    console.log("calculated!")
+    for (var i = 0; i < actionSections.length; i++) {
+        barrier = actionSections[i];
+        actionSectionBarriers[i] = {
+            id: barrier.id,
+            start: barrier.getBoundingClientRect().top + document.querySelector("#scrollBox").scrollTop,
+            end: barrier.getBoundingClientRect().bottom + document.querySelector("#scrollBox").scrollTop
+        }
+    }
+}
+calculateBarriers()
+
+function resizeHandler() {
+    calculateBarriers()
+}
+window.onresize = resizeHandler;
+
 function handleScroll(e) {
-    console.log(document.querySelector("#scrollBox").scrollTop)
+    let position = document.querySelector("#scrollBox").scrollTop;
+    for (var i = 0; i < actionSectionBarriers.length; i++) {
+        if (position > actionSectionBarriers[i].start && position < actionSectionBarriers[i].end) {
+            console.log(actionSectionBarriers[i].id);
+        }
+    }
 }
 
 document.querySelector("#scrollBox").addEventListener("scroll", (e) => {handleScroll(e)});
+
