bad5adbe71022910b636c5584a9c9ce0ba5956ff
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-07-08 15:21:00 -0400

    add test vector and calculations

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ let actionSectionBarriers = []
 
 let actionSections = document.querySelectorAll(".actionSection")
 
+const SCROLLSPEED = 1;
+
+class Pos {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+function calcTravel(start, end) {
+    if (!(start instanceof Pos && end instanceof Pos)) {
+        throw new TypeError("\"start\" and \"end\" must be of Pos class");
+    }
+    return Math.sqrt(((end.x - start.x)**2) + ((end.y - start.y)**2))
+}
+
+const ZONEDATA = [
+                    {id: "zone1", startPos: new Pos(300, 30), endPos: new Pos(300, 225)},
+                    {id: "zone2", startPos: new Pos(350, 300), endPos: new Pos(650, 625)},
+                    {id: "zone3", startPos: new Pos(350, 600), endPos: new Pos(50, 925)}
+                ]
+
+console.log(calcTravel(new Pos(2, 0), new Pos(0, 2)))
+                
 function calculateBarriers() {
     console.log("calculated!")
     for (var i = 0; i < actionSections.length; i++) {
@@ -23,9 +47,20 @@ function resizeHandler() {
 window.onresize = resizeHandler;
 
 function horizScroll(activeSection, position, interPosition) {
-    console.log(activeSection.start + " " + activeSection.end + " " + position);
-    document.getElementById("debug").innerHTML = activeSection.id
-    document.getElementById("debug").style.transform = "translateX(" + interPosition/3 + "px)";
+    let progression = interPosition / (activeSection.end - activeSection.start)
+    let zoneData = null;
+    for (var i = 0; i < ZONEDATA.length; i++) {
+        if (ZONEDATA[i].id == activeSection.id) {
+            zoneData = ZONEDATA[i];
+        }
+    }
+    if (zoneData == null) {
+        return;
+    }
+    let x = zoneData.startPos.x + ((zoneData.endPos.x - zoneData.startPos.x) * progression)
+    let y = zoneData.startPos.y + ((zoneData.endPos.y - zoneData.startPos.y) * progression)
+    console.log(x + " " + y)
+    document.getElementById("stickyViewport").style.backgroundPosition = -x + "vw " + -y + "vh"
 }
 
 
